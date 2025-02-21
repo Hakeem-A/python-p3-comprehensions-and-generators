6f9cd3bfc80a94050c7f7ace856126d1f9dc9698
Abdi Mohamed <bashanesix@gmail.com>
2025-02-21 10:27:23 +0300

    all tests passed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -5,4 +5,5 @@ def return_evens(num_list):
 print(return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence +"!" for sentence in sentence_list]
+print (make_exclamation(["hello", "How are you", "Python is fun"]))
